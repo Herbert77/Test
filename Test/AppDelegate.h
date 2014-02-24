39cb6a4a7a42ad00805acf7733a98043fb30e980
Herbert77 <Herbert@Herbert.local>
2014-02-24 21:52:41 +0800

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 胡 传业 on 14-2-24.
+//  Copyright (c) 2014年 NewThread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test
+//
+//  Created by 胡 传业 on 14-2-24.
+//  Copyright (c) 2014年 NewThread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
